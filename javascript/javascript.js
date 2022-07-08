a4acfdbcd4c45b00f8b1d6a682f99bfe213f68a3
bluff <bluffyx91@gmail.com>
2022-07-08 11:51:37 +0300

    Added the forth and last method

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,3 +1,4 @@
+var auxScore=0;
 function computerPlay (){
     array = ["rock", "paper", "scissors"];
     return array[Math.floor((Math.random()*3))];
@@ -13,27 +14,64 @@ function playerSelection(){
     } else if (newText==="scissors"){
         return newText; 
     } else {
-        alert("You have given the wron input")
+        alert("You have given the wrong  input")
+        auxScore=0;
     }
 }
 
 function singleRound (){
     let computerChoice = computerPlay();
     let playerChoice = playerSelection();
+
     if (computerChoice==playerChoice){
         alert("You have both picked "+ playerChoice+ " It's a tie");
+        auxScore=0;
+      
     } else if (computerChoice=="rock" && playerChoice=="scissors"){
         alert("You Lose! Rock beats scissors");
+        auxScore = -1;
     } else if (computerChoice=="rock" && playerChoice=="paper"){
         alert("You Win! Paper beats rock");
+        auxScore = 1;
     } else if (computerChoice=="paper" && playerChoice=="scissors"){
         alert("You win! Scissors beats paper");
+        auxScore = 1;
     } else if (computerChoice=="paper" && playerChoice=="rock"){
         alert("You lose! Paper beats rock");
+        auxScore = -1;
     } else if (computerChoice=="scissors" && playerChoice=="rock"){
         alert("You win! Rock beats scissors");
+        auxScore = +1;
     } else if (computerChoice=="scissors" && playerChoice=="paper"){
         alert("You lose! Scissors beats paper");
+        auxScore = -1;
+    }
+    
+    
+}
+
+function game (){
+    let scorePlayer=0;
+    let scoreComputer=0;
+    for (let i=0; i<5; i++){
+        singleRound()
+        
+        if (auxScore===1){
+            scorePlayer=scorePlayer+1;  
+        } else if(auxScore===-1){
+            scoreComputer=scoreComputer+1; 
+        } else if (auxScore===0){
+            scorePlayer=scorePlayer+0;
+            scoreComputer=scoreComputer+0;
+        }
+    }
+    console.log("after 5 games, the score is: computer:"+scoreComputer+" and player: "+scorePlayer);
+    if (scorePlayer > scoreComputer){
+        alert("Congratulations, you won "+scorePlayer+" to "+scoreComputer);
+    } else if (scorePlayer < scoreComputer) {
+        alert("Sorry, you lost "+scorePlayer+" to "+scoreComputer);
+    } else {
+        alert("It's a tie !")
     }
 }
-singleRound();
+game();
