f4e5c818d13245c6321c80eaec65aa37deb6995f
bluff <bluffyx91@gmail.com>
2022-07-16 13:19:18 +0300

    Added 3 buttons with eventListeners and modified the js code

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,77 +1,74 @@
-var auxScore=0;
+let computerScore=0;
+let playerScore=0;
 function computerPlay (){
     array = ["rock", "paper", "scissors"];
     return array[Math.floor((Math.random()*3))];
 }
 
-function playerSelection(){
-    let text = window.prompt("Input your choice: rock / paper / scrissors");
-    let newText = text.toLowerCase();
-    if (newText==="rock"){
-        return newText;
-    } else if (newText==="paper"){
-        return newText;
-    } else if (newText==="scissors"){
-        return newText; 
-    } else {
-        alert("You have given the wrong  input")
-        auxScore=0;
-    }
-}
+const body = document.querySelector('body');
+const div = document.createElement('div');
+div.classList.add('div');
+div.textContent='glorious content...';
+div.style.marginTop='20px';
+
+body.appendChild(div);
 
 function singleRound (){
     let computerChoice = computerPlay();
-    let playerChoice = playerSelection();
 
     if (computerChoice==playerChoice){
-        alert("You have both picked "+ playerChoice+ " It's a tie");
+        div.textContent="You have both picked "+ playerChoice+ " It's a tie";
         auxScore=0;
       
     } else if (computerChoice=="rock" && playerChoice=="scissors"){
-        alert("You Lose! Rock beats scissors");
-        auxScore = -1;
+        computerScore++;
+        div.textContent=`You Lose! Rock beats scissors; score is: Player: ${playerScore} to ${computerScore}`;
     } else if (computerChoice=="rock" && playerChoice=="paper"){
-        alert("You Win! Paper beats rock");
-        auxScore = 1;
+        playerScore++
+        div.textContent=`You Win! Paper beats rock; score is: Player: ${playerScore} to ${computerScore}`;
+        
     } else if (computerChoice=="paper" && playerChoice=="scissors"){
-        alert("You win! Scissors beats paper");
-        auxScore = 1;
+        playerScore++
+        div.textContent=`You win! Scissors beats paper; score is: Player: ${playerScore} to ${computerScore}`;
+        
     } else if (computerChoice=="paper" && playerChoice=="rock"){
-        alert("You lose! Paper beats rock");
-        auxScore = -1;
+        computerScore++;
+        div.textContent=`You lose! Paper beats rock; score is: Player: ${playerScore} to ${computerScore}`;
+        
     } else if (computerChoice=="scissors" && playerChoice=="rock"){
-        alert("You win! Rock beats scissors");
-        auxScore = +1;
+        playerScore++
+        div.textContent=`You win! Rock beats scissors; score is: Player: ${playerScore} to ${computerScore}`;
+        
     } else if (computerChoice=="scissors" && playerChoice=="paper"){
-        alert("You lose! Scissors beats paper");
-        auxScore = -1;
+        computerScore++;
+        div.textContent=`You lose! Scissors beats paper; score is: Player: ${playerScore} to ${computerScore}`;
+        
     }
     
-    
+    if (computerScore == 5 || playerScore == 5){
+        if (computerScore> playerScore){
+            div.textContent="Sorry, you lost "+computerScore+" to "+playerScore;
+        } else {
+            div.textContent=`Congrats, you WON ${playerScore} to ${computerScore}`
+        }
+        computerScore=0;
+        playerScore=0;
+    }
 }
 
-function game (){
-    let scorePlayer=0;
-    let scoreComputer=0;
-    for (let i=0; i<5; i++){
-        singleRound()
+let playerChoice=null;
+const select = document.querySelectorAll('button');
+select.forEach(i => i.addEventListener('click', function(e){
+    playerChoice = i.getAttribute('class');
+        singleRound();
+    
+}))
+
+    
         
-        if (auxScore===1){
-            scorePlayer=scorePlayer+1;  
-        } else if(auxScore===-1){
-            scoreComputer=scoreComputer+1; 
-        } else if (auxScore===0){
-            scorePlayer=scorePlayer+0;
-            scoreComputer=scoreComputer+0;
-        }
-    }
-    console.log("after 5 games, the score is: computer:"+scoreComputer+" and player: "+scorePlayer);
-    if (scorePlayer > scoreComputer){
-        alert("Congratulations, you won "+scorePlayer+" to "+scoreComputer);
-    } else if (scorePlayer < scoreComputer) {
-        alert("Sorry, you lost "+scorePlayer+" to "+scoreComputer);
-    } else {
-        alert("It's a tie !")
-    }
-}
-game();
+    
+    
+    
+    
+
+
